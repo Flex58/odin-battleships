b2bf4f9b0e5f9ab7860e3f7ea4d0923892c6d86d
Flex58 <164533598+Flex58@users.noreply.github.com>
2025-01-30 11:24:25 +0100

    fix: prevent ship to be placed on same spot

--- src/objects/Gameboard.js
+++ src/objects/Gameboard.js
@@ -6,25 +6,38 @@ class Gameboard {
     }
   }
 
-  place(ship, coordW, coordH, mode = "vert"){
+  place(ship, coordW, coordH, mode = "vert") {
     if (mode == "vert") {
-        //we check with -1 for out of area because we count inclusive (5+6 = 5,6,7,8,9,10)
-        if (ship.length + coordH - 1 > 10) {
-            throw new Error("Out of Area")
+      //we check with -1 for out of area because we count inclusive (5+6 = 5,6,7,8,9,10)
+      if (ship.length + coordH - 1 > 10) {
+        throw new Error("Out of Area");
+      }
+
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[coordW][coordH + i]) {
+          throw new Error("Ship already placed");
         }
-        for (let i = 0; i < ship.length; i++){
-            this.board[coordW][coordH + i] = ship
+      }
+
+      for (let i = 0; i < ship.length; i++) {
+        this.board[coordW][coordH + i] = ship;
+      }
+      return this.board[coordW][coordH];
+    } else if (mode == "hori") {
+      if (ship.length + coordW - 1 > 10) {
+        throw new Error("Out of Area");
+      }
+
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[coordW][coordH + i]) {
+          throw new Error("Ship already placed");
         }
-        return this.board[coordW][coordH]
-    }
-    else if(mode == "hori") {
-        if (ship.length + coordW - 1> 10) {
-            throw new Error("Out of Area")
-        }
-        for (let i = 0; i < ship.length; i++){
-            this.board[coordW + i][coordH] = ship
-        }
-        return this.board[coordW][coordH]
+      }
+
+      for (let i = 0; i < ship.length; i++) {
+        this.board[coordW + i][coordH] = ship;
+      }
+      return this.board[coordW][coordH];
     }
   }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -8,11 +8,15 @@ test("Gameboard intialized", () => {
 })
 
 test("Place Ships on Board", () => {
-    expect(board.place(new Ship(2), 5, 6)).toBe(board.board[5][6])
+    expect(board.place(new Ship(2), 7, 6)).toBe(board.board[7][7])
 })
 
 test("Place Ships on Board Horizontal", () => {
     expect(board.place(new Ship(2), 5, 6, "hori")).toBe(board.board[6][6])
+})
+
+test("Place Ships on another Ship", () => {
+    expect(() => board.place(new Ship(2), 5, 6, "hori")).toThrow("Ship already placed")
 })
 
 test("Place Ship out of Bounds", () => {
@@ -21,4 +25,8 @@ test("Place Ship out of Bounds", () => {
 
 test("Place Ship out of Bounds Horizontal", () => {
     expect(() => board.place(new Ship(2), 10, 6, "hori")).toThrow("Out of Area")
+})
+
+test("Recieve Attack hit", () => {
+
 })
